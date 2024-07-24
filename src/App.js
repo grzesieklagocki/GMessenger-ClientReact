53eae693cc57ac21d44b8f6f1c8d03a362e050e5
Grzegorz Łagocki <grzesieklagocki@gmail.com>
2024-07-24 17:38:16 +0200

    feat: add Conversation component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+
+var message = "";
+var messages = ["Initial message"];
+
+class MessagesView extends Component {
+  state = { message, messages };
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          value={this.state.message}
+          onChange={(e) => {
+            this.setState({ message: e.target.value });
+          }}
+        />
+        <input
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            this.state.messages.push(this.state.message);
+            this.setState({ message: "" });
+          }}
+        ></input>
+        <label>
+          {messages.map((msg) => (
+            <div>{msg}</div>
+          ))}
+        </label>
+      </form>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-
+      <MessagesView message={message} messages={messages} />
     </div>
   );
 }
