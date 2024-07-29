313ad9ba87ce49935798e2587fca856814474542
Grzegorz Łagocki <grzesieklagocki@gmail.com>
2024-07-29 02:17:22 +0200

    feat: add logic for sending and receiving messages from server in Conversation component

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -1,20 +1,40 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import MessageTextInput from "./MessageTextInput";
+import WebSocketConnection from "../connection/WebSocketConnection";
 
 function Conversation() {
   const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
 
-  function handleSendMessage(text) {
-    setMessages([...messages, text]); // add latest message
-    console.log("Wysłano:", text);
-  }
+  const server = useRef();
+  const addMessage = useCallback((msg) => {
+    setMessages((messages) => [...messages, msg]);
+  }, []);
+
+  useEffect(() => {
+    server.current = new WebSocketConnection("localhost", 8080, addMessage);
+
+    return () => server.current.close();
+  }, [addMessage]);
+
+  useEffect(() => {
+    if (!message) return;
+
+    addMessage(message);
+    server.current.sendMessage(message);
+  }, [message, addMessage]);
 
   return (
     <div>
-      <MessageTextInput onMessageSend={handleSendMessage} />
-      {messages.map((msg) => (
-        <div>{msg}</div>
-      ))}
+      <div>
+        {messages.map((msg, i) => (
+          <div key={i}>{msg}</div>
+        ))}
+      </div>
+      <MessageTextInput
+        placeholder="Type message..."
+        onSubmit={(msg) => setMessage(msg)}
+      />
     </div>
   );
 }
